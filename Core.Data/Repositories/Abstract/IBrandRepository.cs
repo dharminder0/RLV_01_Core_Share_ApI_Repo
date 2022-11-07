6625051e861eaa4d0819014c45ed59001342b685
Rishabh Sharma <rishabh.sharma@fleekitsolutions.com>
2022-11-07 17:24:05 +0530

    Create API

---
+++ Core.Data/Repositories/Abstract/IBrandRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Business.Entites.DataModels;
+using Core.Common.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Repositories.Abstract {
+    public interface IBrandRepository :IDataRepository<Brand>{
+        IEnumerable<Brand> GetBrand();
+    }
+}
