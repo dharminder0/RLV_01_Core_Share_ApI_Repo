95edaaeeeac657da944883ef37475fbde496ceee
Roushan Kumar <roushan.kumar@fleekitsolutions.com>
2022-11-07 11:41:52 +0530

    add a small fixed for reference

--- Core.Business.Entites/DataModels/Hospital.cs
+++ Core.Business.Entites/DataModels/Hospital.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Common.Data;
 
-namespace Core.Business.Entites.DataModels {
+namespace Core.Business.Entites.DataModels
+{
     [Alias(Name = "Hospital ")]
     public class Hospital {
         public Hospital() { }
