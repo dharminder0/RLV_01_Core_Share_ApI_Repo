3dd9cbcde122772f45dbfe9d9d8fca83527eab96
Rishabh Sharma <rishabh.sharma@fleekitsolutions.com>
2022-11-07 14:08:39 +0530

    Add Models

--- Core.Business.Entites/DataModels/Country.cs
+++ Core.Business.Entites/DataModels/Country.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.Business.Entites.DataModels {
+﻿namespace Core.Business.Entites.DataModels {
     public class Country {
         public Country () { }
         public int Id { get; set; }
---
+++ Core.Business.Entites/DataModels/DoctorHospitalRef.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Business.Entites.DataModels {
+    public class DoctorHospitalRef {
+        public DoctorHospitalRef() { }
+        public int Id { get; set; }
+        public int HospitalId { get; set; }    
+        public int DoctorUserId { get; set; }
+        public bool IsPersonalClinic { get; set; }
+
+    }
+}
