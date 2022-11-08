fbfe7096e8576880ca6239ed460503217edfe92d
Sumit <sumit@fleekitsolutions.com>
2022-11-08 12:57:50 +0530

    Create get api for Doctor and doctor details

---
+++ Core.Data/Repositories/Abstract/IDoctorRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Business.Entites.DataModels;
+using Core.Common.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Repositories.Abstract
+{
+    public interface IDoctorRepository:IDataRepository<Doctor>
+    {
+        IEnumerable<Doctor> GetDoctors();
+        IEnumerable<Doctor> GetDoctorById(int Userid);
+    }
+}
