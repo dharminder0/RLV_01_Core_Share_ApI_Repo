5a54f3c9820eaa7953a3c8344c142ff78d9679e1
Rishabh Sharma <rishabh.sharma@fleekitsolutions.com>
2022-11-11 20:09:05 +0530

    Create Api For SpecialityId

--- Core.Business/Services/Abstract/ITreatmentService.cs
+++ Core.Business/Services/Abstract/ITreatmentService.cs
@@ -1,5 +1,9 @@
-﻿namespace Core.Business.Services.Abstract {
+﻿using Core.Business.Entites.DataModels;
+using Core.Business.Entites.RequestModels;
+
+namespace Core.Business.Services.Abstract {
     public interface ITreatmentService {
         object TreatmentInfoById(string countryCode);
+        List<Treatment> TreatmentInfoBySpecialityId(TreatmentRequest treatmentRequest);
     }
 }
