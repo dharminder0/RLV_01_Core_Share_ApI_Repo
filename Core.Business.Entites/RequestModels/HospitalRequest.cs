c11af2fb3f28ca5b097fc3b2fadec6739d87f1f4
Sumit <sumit@fleekitsolutions.com>
2022-11-14 20:30:48 +0530

    add Filters

--- Core.Business.Entites/RequestModels/HospitalRequest.cs
+++ Core.Business.Entites/RequestModels/HospitalRequest.cs
@@ -1,10 +1,26 @@
-﻿namespace Core.Business.Entites.RequestModels {
-    public class HospitalRequest {
+﻿namespace Core.Business.Entites.RequestModels
+{
+    public class HospitalRequest
+    {
         //[AllowNull]
         public string SearchText { get; set; } = "";
-        public string CountryCode { get; set; }
-        public List<int> CityList { get; set; }
-        public List<int> HospitalList { get; set; }
+        public string CountryCode { get; set; } = "IN";
+        public List<int>? CityList { get; set; }
+        public List<int>? HospitalList { get; set; }
         public int LanguageId { get; set; } = 1;
+        public List<int>? EstablishedYear { get; set; }
+        public List<int>? BedCount { get; set; }
+        public List<int>? specialityId { get; set; }
+        public List<int>? TreatmentIds { get; set; }
+        public int PageIndex { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+
     }
+    //    public class EstablishedYear
+    //    {
+    //        public int MinValue { get; set; } = 1980;
+    //        public int MaxValue { get; set; } = 2000;
+
+    //    }
 }
+//}
