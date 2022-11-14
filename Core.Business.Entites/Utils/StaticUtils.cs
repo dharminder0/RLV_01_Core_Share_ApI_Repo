befd233257bfcc6243efb278d96e7d540d875710
Roushan Kumar <roushan.kumar@fleekitsolutions.com>
2022-11-14 19:44:47 +0530

    add a new blobController

---
+++ Core.Business.Entites/Utils/StaticUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Business.Entites.Utils {
+    public static class StaticUtils {
+        public static string ExtractInnerException(this Exception ex) {
+            string errorMessage = String.Empty;
+            if (ex.Message != null) {
+                errorMessage = $"/Msg/-{ex.Message}{Environment.NewLine}";
+                if (ex.Source != null) {
+                    errorMessage += $"/Src/-{ex.Source}{Environment.NewLine}";
+                }
+                if (ex.StackTrace != null) {
+                    errorMessage += $"/Stack/-{ex.StackTrace}{Environment.NewLine}";
+                }
+            }
+            return errorMessage;
+        }
+    }
+}
---
+++ Core.Business/Services/Concrete/BlobStorageService.cs
@@ -0,0 +1,70 @@
+﻿using Core.Common.Settings;
+using Microsoft.WindowsAzure.Storage;
+
+namespace Core.Business.Services.Concrete {
+    public class BlobStorageService {
+
+        private readonly static string _blobStorageAccount = GlobalSettings.BlobStorageAccount;
+        private readonly static string _blobContainerName = GlobalSettings.BlobContainerName;
+        public string UploadFileToAzureBlob(string fileIdentifier, byte[] fileData) {
+            try {
+                string uri = null;
+                var directoryName = "";
+                // Retrieve storage account from connection string.
+                var storageAccount = CloudStorageAccount.Parse(_blobStorageAccount);
+
+                // Create the blob client.
+                var blobClient = storageAccount.CreateCloudBlobClient();
+
+                // Retrieve reference to a previously created container.
+                var container = blobClient.GetContainerReference(_blobContainerName);
+                var resourceContainer = !string.IsNullOrWhiteSpace(directoryName)
+                    ? directoryName + "/" + fileIdentifier
+                    : fileIdentifier;
+
+                var blockBlob = container.GetBlockBlobReference(resourceContainer);
+
+                //Todo : Dharminder  MimeMapping.GetMimeMapping
+                // blockBlob.Properties.ContentType = MimeMapping.GetMimeMapping(fileIdentifier);
+                blockBlob.UploadFromByteArrayAsync(fileData, 0, fileData.Length);
+
+                uri = blockBlob.Uri.AbsoluteUri;
+                return uri;
+            } catch (Exception) { return null; }
+        }
+
+        public string GetBlobLinkByFileIdentifier(string fileIdentifier) {
+            var directoryName = "";
+
+            // Retrieve storage account from connection string.
+            var storageAccount = CloudStorageAccount.Parse(_blobStorageAccount);
+
+            // Create the blob client.
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve reference to a previously created container.
+            var container = blobClient.GetContainerReference(_blobContainerName);
+            var resourceContainer = !string.IsNullOrWhiteSpace(directoryName)
+                ? directoryName + "/" + fileIdentifier
+                : fileIdentifier;
+
+            var blockBlob = container.GetBlockBlobReference(resourceContainer);
+            return blockBlob.Uri.AbsoluteUri;
+        }
+
+        public FileUploadResponseModel UploadFileToBlob(string fileName, byte[] fileData) {
+            FileUploadResponseModel response = null;
+            var fileIdentifier = GetUniqueFileName(fileName);
+
+            string uri = UploadFileToAzureBlob(fileIdentifier, fileData);
+            if (!string.IsNullOrWhiteSpace(uri)) {
+                response = new FileUploadResponseModel();
+                response.FileName = fileName;
+                response.FileIdentifier = fileIdentifier;
+                response.FileLink = uri;
+            }
+            return response;
+        }
+
+    }
+}
