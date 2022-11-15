a591dd7ea113c482a052bd27d79de725ff7b3faa
Roushan Kumar <roushan.kumar@fleekitsolutions.com>
2022-11-15 18:22:29 +0530

    add a new api

---
+++ Core.Data/Repositories/Concrete/SpecialityRepository.cs
@@ -0,0 +1,17 @@
+﻿using Core.Business.Entites.DataModels;
+using Core.Common.Data;
+using Core.Data.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Data.Repositories.Concrete {
+    public class SpecialityRepository : DataRepository<Speciality>, ISpecialityRepository {
+        public IEnumerable<Speciality> GetSpecialityInfo(string countryCode) {
+            var sql = $@"select * from Speciality where LanguageId = @countryCode";
+            return Query<Speciality>(sql, new { countryCode });
+        }
+    }
+}
